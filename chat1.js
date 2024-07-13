bdca7014ae91d8ab0f2a82094902a7dd1c88c358
chatty-lol <help.chattylol@gmail.com>
2024-07-13 16:21:44 +0200

    Update chat1.js

--- chat1.js
+++ chat1.js
@@ -7,6 +7,10 @@ function createElementFromHTML(htmlString) {
   return div.firstElementChild; // Use firstElementChild to ensure an element node is returned
 }
 
+function open_profile(id){
+  
+}
+
 function switch_trig(titles) {
   var stage = this.getAttribute("stage");
   switch(stage) {
@@ -56,8 +60,43 @@ function new_switch(title, stages, max_stages, caller) {
   document.getElementById("left-side-list").appendChild(element);
 }
 
+function self_new_msg(name, profileurl, msg, id){
+  var html = `
+                <div profile_id="${id}" class="chat-msg col-start-1 col-end-8 p-3 rounded-lg">
+                  <div class="flex flex-row items-center">
+                    <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      <img src="${profileurl}" alt="${name.charAt(0)}" items-center justify-center>
+                    </div>
+                    <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                      <div>${msg}</div>
+                    </div>
+                  </div>
+                </div>
+  `
+}
+
+function receive_new_msg(name,profileurl, msg, id){
+  var html = `
+                <div profile_id="${id}" class="chat-msg col-start-6 col-end-13 p-3 rounded-lg">
+                  <div class="flex items-center justify-start flex-row-reverse">
+                    <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                      <img src="${profileurl}" alt="${name.charAt(0)}" items-center justify-center>
+                    </div>
+                    <div class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+                      <div>${msg}</div>
+                    </div>
+                  </div>
+                </div>
+  `
+  var element = createElementFromHTML(html);
+  document.getElementById("left-side-list").appendChild(element);
+}
 
 
 document.addEventListener("DOMContentLoaded", function() {
   new_switch("Status", 'stage_0="flex flex-col justify-center h-4 w-8 rounded-full bg-gray-400 items-start" stage_1="flex flex-col justify-center h-4 w-8 rounded-full bg-indigo-500 items-center" stage_2="flex flex-col justify-center h-4 w-8 rounded-full bg-red-600 items-end"', "3", switch_trig);
+
+  self_new_msg("Person1", "x", "Hello there!","0")
 });
+
+
