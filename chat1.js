363e1eae30f22d0821f894d4825b255885dfb886
chatty-lol <help.chattylol@gmail.com>
2024-07-15 12:45:53 +0200

    Update chat1.js

--- chat1.js
+++ chat1.js
@@ -7,11 +7,65 @@ function createElementFromHTML(htmlString) {
   return div.firstElementChild; // Use firstElementChild to ensure an element node is returned
 }
 
-function open_profile(id){
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+function u_open_profile(id){
   
 }
 
-function switch_trig(titles) {
+function u_status_code_popup(code, api){
+  var el = document.getElementById("status-code-popup")
+  //invisible flex absolute left-0 top-0 w-screen h-screen bg-none items-center justify-center
+  
+  // api switch
+  // 0 = dogs, 1 = cats, 2 = ducks, 3 = goats
+  var urlimg = undefined
+  
+  switch(api){
+    case 0:
+      urlimg = `https://http.dog/${code}.jpg`
+      break;
+    case 1:
+      urlimg =  `https://httpcats.com/${code}.jpg`
+      break;
+    case 2:
+      urlimg = `https://httpducks.com/${code}.jpg`
+      break;
+    case 3:
+      urlimg = `https://httpgoats.com/${code}.jpg`
+      break
+    default:
+      urlimg = `https://httpcats.com/${code}.jpg`
+      break;
+  }
+  el.children[0].children[0].src = urlimg
+  
+  el.className = "flex absolute left-0 top-0 w-screen h-screen bg-none items-center justify-center"
+
+  return urlimg
+}
+
+function u_close_status_code_popup(){
+  var el = document.getElementById("status-code-popup")
+  //invisible flex absolute left-0 top-0 w-screen h-screen bg-none items-center justify-center
+  el.className = "invisible flex absolute left-0 top-0 w-screen h-screen bg-none items-center justify-center"
+}
+
+function u_status_code(code){
+  var rint = randomIntFromInterval(0,3)
+  var ur = u_status_code_popup(code,rint)
+  setTimeout(()=>{
+    if (document.getElementById("status-code-popup").children[0].children[0].src == ur){
+      u_close_status_code_popup()
+    }
+  },5000)
+}
+
+
+function u_switch_trig(titles) {
   var stage = this.getAttribute("stage");
   switch(stage) {
     case "0":
@@ -31,11 +85,11 @@ function switch_trig(titles) {
   this.nextElementSibling.innerText = titles[statusIndex];
 }
 
-function self_name(name) {
+function u_self_name(name) {
   document.getElementById("self-name").innerText = name;
 }
 
-function self_status(status) {
+function u_self_status(status) {
   document.getElementById("self-status").innerText = status;
 }
 
@@ -43,7 +97,7 @@ function get_switches() {
   return document.getElementsByClassName("status-switch");
 }
 
-function new_switch(title, stages, max_stages) {
+function u_new_switch(title, stages, max_stages) {
   var html = `
     <div class="status-switch flex flex-row items-center mt-3">
       <div class="leading-none mr-1 text-xs font-bold">${title}</div>
@@ -55,12 +109,12 @@ function new_switch(title, stages, max_stages) {
   `;
   var element = createElementFromHTML(html);
   element.querySelector('.cursor-pointer').addEventListener('click', function() {
-    switch_trig.call(this, ["Invisible", "Online", "Do Not Disturb"]);
+    u_switch_trig.call(this, ["Invisible", "Online", "Do Not Disturb"]);
   });
   document.getElementById("left-side-list").appendChild(element);
 }
 
-function receive_new_msg(name, profileurl, msg, id){
+function u_receive_new_msg(name, profileurl, msg, id){
   var html = `
                 <div profile_id="${id}" class="chat-msg col-start-1 col-end-8 p-3 rounded-lg">
                   <div class="flex flex-row items-center">
@@ -77,7 +131,7 @@ function receive_new_msg(name, profileurl, msg, id){
   document.getElementById("messages-content").appendChild(element);
 }
 
-function send_new_msg(name,profileurl, msg, id){
+function u_send_new_msg(name,profileurl, msg, id){
   var html = `
                 <div  profile_id="${id}" class="chat-msg col-start-6 col-end-13 p-3 rounded-lg">
                   <div class="flex items-center justify-start flex-row-reverse">
@@ -97,19 +151,21 @@ function send_new_msg(name,profileurl, msg, id){
   return element
 }
 
-function self_msg_status(el, status){
+function u_self_msg_status(el, status){
   var st_el = el.children[0].children[1].children[1].innerText = status
 }
 
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  new_switch("Status", 'stage_0="flex flex-col justify-center h-4 w-8 rounded-full bg-gray-400 items-start cursor-pointer" stage_1="flex flex-col justify-center h-4 w-8 rounded-full bg-indigo-500 items-center cursor-pointer" stage_2="flex flex-col justify-center h-4 w-8 rounded-full bg-red-600 items-end cursor-pointer"', "3");
+  u_new_switch("Status", 'stage_0="flex flex-col justify-center h-4 w-8 rounded-full bg-gray-400 items-start cursor-pointer" stage_1="flex flex-col justify-center h-4 w-8 rounded-full bg-indigo-500 items-center cursor-pointer" stage_2="flex flex-col justify-center h-4 w-8 rounded-full bg-red-600 items-end cursor-pointer"', "3");
 
-  receive_new_msg("Oki", "x", "Allo", "0")
+  u_receive_new_msg("Oki", "x", "Allo", "0")
   
-  var mymsg = send_new_msg("Person1", "x", "Hello there!","0")
-  self_msg_status(mymsg, "Seen")
+  var mymsg = u_send_new_msg("Person1", "x", "Hello there!","0")
+  u_self_msg_status(mymsg, "Seen")
+
+  u_status_code(200)
 });
 
 
