dc193cc627ab961c293f298840aec11e25ee49ea
chatty-lol <help.chattylol@gmail.com>
2024-07-15 13:32:17 +0200

    Update chat1.js

--- chat1.js
+++ chat1.js
@@ -157,13 +157,15 @@ function u_self_msg_status(el, status){
 
 document.addEventListener("DOMContentLoaded", function() {
   u_new_switch("Status", 'stage_0="flex flex-col justify-center h-4 w-8 rounded-full bg-gray-400 items-start cursor-pointer" stage_1="flex flex-col justify-center h-4 w-8 rounded-full bg-indigo-500 items-center cursor-pointer" stage_2="flex flex-col justify-center h-4 w-8 rounded-full bg-red-600 items-end cursor-pointer"', "3");
+  u_new_switch("Server Responses", 'stage_0="flex flex-col justify-center h-4 w-8 rounded-full bg-green-600 items-start cursor-pointer" stage_1="flex flex-col justify-center h-4 w-8 rounded-full bg-red-600 items-center cursor-pointer"', "2");
 
+  
   u_receive_new_msg("Oki", "x", "Allo", "0")
   
   var mymsg = u_send_new_msg("Person1", "x", "Hello there!","0")
   u_self_msg_status(mymsg, "Seen")
 
-  u_status_code(200)
+  u_status_code(100)
 });
 
 
